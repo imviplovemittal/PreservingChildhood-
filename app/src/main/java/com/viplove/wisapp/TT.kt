84bf06200e516b45244d63a81ec48130b7fddafc
imviplovemittal <viplovemittal2@gmail.com>
2018-01-28 07:33:17 +0530

    Delete TT.kt

--- app/src/main/java/com/viplove/wisapp/TT.kt
+++
@@ -1,6 +0,0 @@
-package com.viplove.wisapp
-
-/**
- * Created by viplo on 23-Jan-18.
- */
-data class TT(val sTime: String,val slot: String,val eTime: String,val subNmae: String,val roomno: String,val faculty: String,val batch : String)
